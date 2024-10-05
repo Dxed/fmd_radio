2db9d578bfa3f16dccb60d428bc2d5c6f43702d2
Dxed <53851831+Dxed@users.noreply.github.com>
2024-10-05 17:08:22 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,12 @@
+function updateClock() {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+
+        document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+// Обновляем часы каждую секунду
+setInterval(updateClock, 1000);
+updateClock(); // Вызываем один раз, чтобы сразу отобразить время
